116e652e12a3c443712557c37bad46ceb5f663fc
cece973228 <cedricdu973@gmail.com>
2021-11-09 01:03:34 +0100

    modifier mdp

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ProfileController extends AbstractController
 {
@@ -37,11 +38,36 @@ class ProfileController extends AbstractController
             $em->flush();
 
             $this->addFlash('message', 'Profil mis à jour');
-            return $this->redirectToRoute('users');
+            return $this->render('profil/profil.html.twig');
         }
 
         return $this->render('profil/editprofile.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/users/pass/modifier", name="users_pass_modifier")
+     */
+    public function editPass(Request $request, UserPasswordHasherInterface $password)
+    {
+        if ($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $this->getUser();
+
+            // On vérifie si les 2 mots de passe sont identiques
+            if ($request->request->get('pass') == $request->request->get('pass2')) {
+                $user->setPassword($password->hashPassword($user, $request->request->get('pass')));
+                $em->flush();
+                $this->addFlash('message', 'Mot de passe mis à jour avec succès');
+
+                return $this->render('profil/profil.html.twig');
+            } else {
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques');
+            }
+        }
+
+        return $this->render('profil/editmdp.html.twig');
     }
 }
